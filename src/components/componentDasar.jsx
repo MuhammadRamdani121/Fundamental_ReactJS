5ff237fd022496ae86266c67ad33e49878d6ce63
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-03-30 20:23:28 +0700

    Menambahkan Folder 1.component dasar

--- src/components/componentDasar.jsx
+++ src/components/componentDasar.jsx
@@ -1,7 +1,14 @@
+import img1 from "../assets/1.png";
 export default function ComponentDasar() {
   return (
-    <div>
-      <h1>Component Dasar Pertama</h1>
+    <div className="">
+      <header className="text-center text-2xl font-bold">
+        <h1>1.Dasar components </h1>
+      </header>
+      <main className="flex justify-around items-center">
+        <h1>Component Dasar Pertama</h1>
+        <img src={img1} />
+      </main>
     </div>
   );
 }
