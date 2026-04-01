8cf68b560aaae5cd7d40ad020bb93820c7d12efd
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-04-01 23:47:46 +0700

    Memperbarui Fitur Search

--- src/components/postjson/main.jsx
+++ src/components/postjson/main.jsx
@@ -8,12 +8,13 @@ export default function MainJson() {
   return (
     <>
       <h1 className="mt-4 text-center text-2xl font-bold">4.Props Json</h1>
-      <div>
-        Cari Artikel :<input onChange={changeSearch}></input>
+      <div className="flex justify-center mt-10">
+        Cari Artikel :
+        <input className="border-2 " onChange={changeSearch}></input>
       </div>
-      {post.map((title, tags, date) => (
+      {post.map(({ title, tags, date }, index) => (
         // Spread Array
-        <PostJson {...{ title, tags, date }} />
+        <PostJson {...{ title, tags, date }} key={index} />
       ))}
     </>
   );
