adf71ee80b988248f862adda49874203a27b1302
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-04-01 23:06:36 +0700

    Menambahkan Fitur Search

--- src/components/postjson/main.jsx
+++ src/components/postjson/main.jsx
@@ -1,11 +1,19 @@
 import post from "../../post.json";
 import PostJson from "../postjson/postJson";
 export default function MainJson() {
+  // Variabel changeSearch
+  const changeSearch = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <>
       <h1 className="mt-4 text-center text-2xl font-bold">4.Props Json</h1>
-      {post.map((blog) => (
-        <PostJson title={blog.title} tags={blog.tags} date={blog.date} />
+      <div>
+        Cari Artikel :<input onChange={changeSearch}></input>
+      </div>
+      {post.map((title, tags, date) => (
+        // Spread Array
+        <PostJson {...{ title, tags, date }} />
       ))}
     </>
   );
