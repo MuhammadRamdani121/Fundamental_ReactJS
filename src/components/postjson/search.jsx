da76a8b893a8819ed97b43be9ebadac896ca5aaf
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-04-03 17:25:19 +0700

    Menambahkan Fungsi onClick Method

--- src/components/postjson/search.jsx
+++ src/components/postjson/search.jsx
@@ -3,20 +3,32 @@ import { useState } from "react";
 export default function Search(props) {
   const [search, setSearch] = useState("");
 
-  const changeSearch = (event) => {
-    setSearch(event.target.value);
-    props.changeSearch(event.target.value);
+  const changeSearch = () => {
+    props.changeSearch(search);
+  };
+
+  const searchKeydown = (e) => {
+    if (e.key === "Enter") {
+      changeSearch();
+    }
   };
 
   return (
     <>
       <div className="flex justify-center mt-10">
         Cari Artikel :
-        <input className="border-2 " onChange={changeSearch}></input>
-        <small>
-          Ditemukan {props.totalPosts} dengan data pencarian kata {search}
-        </small>
+        <input
+          className="border-2 "
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchKeydown}
+        ></input>
+        <button className="ml-4 h-8 w-10 border-2" onClick={changeSearch}>
+          Cari
+        </button>
       </div>
+      <small className="flex justify-center">
+        Ditemukan {props.totalPosts} dengan data pencarian kata {search}
+      </small>
     </>
   );
 }
